c4837bef580c09a100d3e497f4996c792b5c7fc0
tomasgsantos <tomasgsantos@gmail.com>
2026-03-21 12:52:25 +0000

    na: fixed base path

--- apps/consultant-command-center/vite.config.ts
+++ apps/consultant-command-center/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/consultant-command-center/'
+  base: '/nexus-command-monorepo/'
 })
