7fe4da6cfd1336ffe1f3414538f0a8ae419291c0
tomasgsantos <tomasgsantos@gmail.com>
2026-03-31 21:31:00 +0100

    feature/pulse-dashboard: fixed text animation

--- packages/ui/src/TextHoverAnimation.tsx
+++ packages/ui/src/TextHoverAnimation.tsx
@@ -13,12 +13,12 @@ export const TextHoverAnimation = (props: OwnProps) => {
       <div className="text-animation-wrap" style={{ height: `${fontSize + 2}px` }}>
         <div className="lowercase">
           {text?.split('').map((e, i) => (
-              <span key={`${e}-${i}`} style={{ fontSize: `${fontSize}px` }}>{e}</span>
+              <span key={`${e}-${i}`} style={{ fontSize: `${fontSize}px`, margin: `${e === ' ' ? '0 2px' : undefined}` }}>{e}</span>
           ))}
         </div>
         <div className="uppercase">
           {text?.split('').map((e, i) => (
-              <span key={`${e}-${i}`} style={{ fontSize: `${fontSize}px` }}>{e}</span>
+              <span key={`${e}-${i}`} style={{ fontSize: `${fontSize}px`, margin: `${e === ' ' ? '0 2px' : undefined}` }}>{e}</span>
           ))}
         </div>
       </div>
